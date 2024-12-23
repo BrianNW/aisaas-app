078ceb8adeb7fee9bb509364cc07b79709fded80
Brian <briantechdom@gmail.com>
2024-12-22 16:52:21 -0800

    Import and update layout font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = IBM_Plex_Sans({ 
+  subsets: ["latin"], 
+weight: ['400', '500', '600', '700'],
+variable: '--font-ibm-plex-sans' });
 
 export const metadata: Metadata = {
   title: "AI SASS",
-<<<<<<< HEAD
   description: "AI Powered Image Generator",
-=======
-  description: "AI powered image generator",
->>>>>>> 9a8d93f179e5e2309aa43d539b8497456604e1e4
 };
 
 export default function RootLayout({
